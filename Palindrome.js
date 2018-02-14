6181379c4c5a1f826f21873e013e4df8bec841f7
Guest User <Guest@epicodus-97.local>
2018-02-14 09:13:57 -0800

    add functionality to handle all known testcases

--- Palindrome.js
+++ Palindrome.js
@@ -1,10 +1,12 @@
-let word = prompt("Enter a word");
-let newArray = word.split("");
+let word = prompt("Enter a word").toLowerCase();
+let alteredWord = word.split(" ").join("");
+
+let newArray = alteredWord.split("");
 let emptiedArray = newArray.slice(); //this array will be emptied, and original will remain intact
 let newArrayReversed = [];
 
 for(let i = newArray.length; i > 0; i--){
-	let extractedLetter = emptiedArray.pop();
+	let extractedLetter = emptiedArray.pop([i]);
 
   newArrayReversed.push(extractedLetter);
 }
@@ -14,6 +16,3 @@ if(newArray.toString() == newArrayReversed.toString()){
 }else{
 	console.log("Sorry, mate, tough break.");
 }
-
-console.log(newArray);
-console.log(newArrayReversed);
